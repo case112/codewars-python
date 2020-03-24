d4d6406dbd4fd9c59e4f97efbe40f8377c968af2
Oskar <case112@users.noreply.github.com>
2020-03-24 10:23:37 +0200

    Create form_the_largest.py

---
+++ form_the_largest.py
@@ -0,0 +1,23 @@
+# Description:
+# Task
+
+# Given a number , Return _The Maximum number _ could be formed from the digits of the number given .
+# Notes
+
+#     Only Natural numbers passed to the function , numbers Contain digits [0:9] inclusive !alt !alt
+
+#     Digit Duplications could occur , So also consider it when forming the Largest !alt
+
+# Input >> Output Examples:
+
+# maxNumber (213) ==> return (321)
+
+# Explanation:
+
+# As 321 is _The Maximum number _ could be formed from the digits of the number *213*** .
+
+# maxNumber (7389) ==> return (9873)
+
+
+def max_number(n):
+    return int(''.join(sorted(str(n), reverse=True)))
